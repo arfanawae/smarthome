481e63dfe954e066744adc1162848203a79d2c82
arfan <arfanawae084@gmail.com>
2026-08-09 14:48:19 +0700

    อัปเดตปุ่มกด

--- Python/rooms_config.py
+++ Python/rooms_config.py
@@ -2,6 +2,7 @@
 ROOMS = [
     {"id": "kitchen",     "name": "ห้องครัว"},
     {"id": "bedroom",     "name": "ห้องนอน"},
+    {"id": "bedroom2",    "name": "ห้องนอน 2"},
     {"id": "bathroom",    "name": "ห้องน้ำ"},
     {"id": "living_room", "name": "ห้องนั่งเล่น"},
     {"id": "storage",     "name": "ห้องเก็บของ"},
